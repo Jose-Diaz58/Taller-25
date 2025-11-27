7d67248b801d0fbea30a8d25b9db4976d69cf253
Jose Diaz <joseluisdiaztrinidad2@gmail.com>
2025-11-26 18:13:21 -0600

    Funcion restar y sumar

---
+++ Ejemplo.js
@@ -0,0 +1,11 @@
+//Funcion de sumar
+function Sumar(a,b){
+    return a+b;
+}
+
+//Funcion para restar
+const Restar=(a,b)=>{
+    return a-b;
+}
+console.log(Restar(8-4));
+console.log(Sumar(2,4));
