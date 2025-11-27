def34e61147ea2408216417f90ff073ceb610f7f
Jose Diaz <joseluisdiaztrinidad2@gmail.com>
2025-11-26 18:16:50 -0600

    Actualización en Ejemplo.js

--- Ejemplo.js
+++ Ejemplo.js
@@ -7,5 +7,9 @@ function Sumar(a,b){
 const Restar=(a,b)=>{
     return a-b;
 }
+function Dividr(a,b){
+    return a/b;
+}
+console.log(Dividr(8,4));
 console.log(Restar(8-4));
 console.log(Sumar(2,4));
